0505fd20ccce36497b5655d98901a51daa030eb0
RJ <r.j.chomik@gmail.com>
2020-09-18 20:50:13 +0100

    Start implementation, need to rethink the approach as I am not sure if this way I will be able to use CoreData functionality

---
+++ DreamTeamAssigment/NewTeamViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewTeamViewController.swift
+//  DreamTeamAssigment
+//
+//  Created by The App Experts on 18/09/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class NewTeamViewController: UIViewController {
+    @IBOutlet weak var teamNameTF: UITextField!
+    @IBOutlet weak var teamColourTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    
+}
--- DreamTeamAssigment/Tabs/TeamsViewController.swift
+++ DreamTeamAssigment/Tabs/TeamsViewController.swift
@@ -14,9 +14,30 @@ class TeamsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        teamsTableView.dataSource = self
+        teamsTableView.delegate = self
     }
     
     @IBAction func addNewTeam(_ sender: UIButton) {
         print("You tap + team btn")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let addNewTeamViewController = storyboard.instantiateViewController(identifier: "NewTeamViewController") as? NewTeamViewController else { return  }
+        self.navigationController?.pushViewController(addNewTeamViewController, animated: true)
+    }
+}
+
+extension TeamsViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension TeamsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
